615a32c3f215d909b41faa7cf740e67c977d87d1
Remy Pearlstone <remy.j.pearlstone@gmail.com>
2017-12-11 23:26:13 -0700

    normalize

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ function spawnArrows() {
     let container = document.getElementById("container");
     let rightArrow = document.createElement("img");
     rightArrow.setAttribute("src", "images/arrowright.svg");
-    rightArrow.height = "75";
+    rightArrow.className = 'arrows';
     container.appendChild(rightArrow);
     
-   TweenMax.to(rightArrow, 5, {
-       y: 500
-   });
+   TweenMax.fromTo(rightArrow, 5, 
+    {css:{top:'-200px'}},{css:{top:'1000px'}});
    
-    
 }
 
 spawnArrows()
