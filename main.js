22896d2a394b5751be4e6598b49ce9f1d9878eee
rover33 <remy.j.pearlstone@gmail.com>
2017-12-11 18:17:36 -0700

    yay

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function spawnArrows() {
-    console.log("HEYYYY");
-
+    let container = document.getElementById("container");
+    let rightArrow = document.createElement("img");
+    rightArrow.setAttribute("src", "images/arrowright.svg");
+    rightArrow.height = "75";
+    container.appendChild(rightArrow);
+    
+    let fall = new TimelineMax({repeat})
 } 
 
 spawnArrows()
 
-let container = document.getElementById("container");
-let rightArrow = document.createElement("img")
-rightArrow.setAttribute("src", "images/arrowright.svg")
-rightArrow.height = "75";
-container.appendChild(rightArrow);
+
+
