045410f089fb3530f769e559fb834fafd8fe632e
rover33 <remy.j.pearlstone@gmail.com>
2017-12-13 11:39:38 -0700

    arrows all work

--- main.js
+++ main.js
@@ -10,23 +10,56 @@ startGame()
 // audio.play();
 var box1 = document.getElementById('box1');
 var rect = box1.getBoundingClientRect();
+var box2 = document.getElementById('box2');
+var square = box2.getBoundingClientRect();
+var box3 = document.getElementById('box3');
+var triangle = box3.getBoundingClientRect();
+
 
 function startGame(){
     window.onkeydown = function(run){ 
         var leftArrows = document.getElementsByClassName('leftArrows')// creating a new variable that is contains the class leftArrows
+        var upArrows = document.getElementsByClassName('upArrows') // creating new variable that contains class upArrows
+        var rightArrows = document.getElementsByClassName('rightArrows')// creating variable that contains class rightArrows
             if(run.keyCode == 65){ //on pressing 'A' it will click the left arrow
                 if (leftArrows){   //runs through the array of left arrows 
                     for (var i = 0; i < leftArrows.length; i++) {
                         console.log(leftArrows[i])
-                        var arrowPosition = leftArrows[i].getBoundingClientRect(); //gving the array position to a new variable
-                        console.log(arrowPosition)
+                        var arrowPositionLeft = leftArrows[i].getBoundingClientRect(); //giving the array position to a new variable
+                        console.log(arrowPositionLeft)
                         console.log(rect.top)
-                        if(arrowPosition.top < (rect.top + 20) && arrowPosition.top > (rect.top - 20)){ //saying when the arrow is basically in the box
+                        if(arrowPositionLeft.top < (rect.top + 30) && arrowPositionLeft.top > (rect.top - 30)){ //saying when the arrow is basically in the box
                     alert('success')
-                    }
+                    } 
                 }
             }
+        }else if(run.keyCode == 87){ //on pressing 'W' it will click the left arrow
+            if(upArrows){ //runs through the array of  arrows 
+               for (var i = 0; i < upArrows.length; i++) {
+                console.log(upArrows[i])
+                var arrowPositionUp = upArrows[i].getBoundingClientRect();//giving the array position to a new variable
+                console.log(arrowPositionUp)
+                console.log(square.top)
+                if(arrowPositionUp.top < (square.top + 30) && arrowPositionUp.top > (square.top - 30)){ //saying when the arrow is basically in the box
+                    alert('up success')
+                    }
+                } 
+            }
+
+        } else if(run.keyCode == 68){ //on pressing 'D' it will click the left arrow
+            if(rightArrows){ //runs through the array of right arrows 
+               for (var i = 0; i < rightArrows.length; i++) {
+                console.log(upArrows[i])
+                var arrowPositionRight = rightArrows[i].getBoundingClientRect();//giving the array position to a new variable
+                console.log(arrowPositionRight)
+                console.log(triangle.top)
+                if(arrowPositionRight.top < (triangle.top + 30) && arrowPositionRight.top > (triangle.top - 30)){ //saying when the arrow is basically in the box
+                    alert('right success')
+                    }
+                } 
+            }
         }
+
     }    
 
 //    } else if(run.keyCode == 68) {
@@ -38,7 +71,7 @@ function startGame(){
         let rightArrow = document.createElement("img");
         rightArrow.setAttribute("src", "images/arrowright.svg");
         rightArrow.className = 'rightArrows';
-        container.appendChild(rightArrows);
+        container.appendChild(rightArrow);
         
     TweenMax.fromTo(rightArrow, 5, //TweenMax means it is animating my variable rightArrow at the total time it takes to do the animatiion
         {css:{top:'-200px'}},{css:{top:'1000px'}, onComplete:function() {rightArrow.remove()}, onUpdate:update}); //animating from -200px above the screen and then once it reaches the page it goes down 1000px. onComplete tell me when the animation is doen and then removes that arrow from the screen.
